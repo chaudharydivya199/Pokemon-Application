8a83a4c15317c560dfb4e82dde682e0579bd518e
Divya Chaudhary <62565599+chaudharydivya199@users.noreply.github.com>
2023-06-23 13:03:41 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,6 @@ function Footer(){
     return(
         <div className = 'footer flex justify-center my-3'>
             <div className = 'flex justify-around'>
-                <a href = "" target = "_blank">
-                    <img className = "mx-2" width = "28px" height = "28px" src = '/web.png' alt = 'website' />
-                </a>
 
                 <a href = "https://github.com/chaudharydivya199" target = "_blank">
                     <img className = "mx-2" width = "28px" height = "28px" src = '/github.png' alt = 'github' />
